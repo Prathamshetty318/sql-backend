c3e61ee209ad89206b0e3e90dbcc5c652da81ba4
Pratham <110841240+Prathamshetty318@users.noreply.github.com>
2025-12-18 05:49:34 +0530

    Create database and tables for user and orders and order_items
    This script creates the database and tables for user, orders, and order items, including foreign key constraints they are connected to each other along with on delete cascade

--- 01_sql_basics/schema.sql
+++ 01_sql_basics/schema.sql
@@ -1,1 +1,40 @@
 
+--create database
+create database backend_sql_mastery;
+
+--create table user
+create table users(
+id serial primary key,
+name varchar(100) not null,
+email varchar(100) not null,
+created_at timestamp default current_timestamp);
+
+
+--order table 
+create table orders(
+id serial primary key,
+user_id int not null,
+order_date timestamp default current_timestamp,
+total_amount numeric(10,2) not null,
+constraint fk_user
+foreign key (user_id)
+references users(id)
+on delete cascade);
+
+
+--order item table 
+create table order_items(
+id serial primary key,
+order_id int not null,
+product_name varchar(150) not null,
+quantity int not null check (quantity > 0),
+price numeric(10,2) not null check(price > 0 ),
+constraint fk_order
+foreign key (order_id)
+references orders(id)
+on delete cascade);
+
+
+select * from order_items;
+select * from orders;
+select * from users;
